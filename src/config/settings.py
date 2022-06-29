31a1ca8d31eb2fb20f6b364347147b5dd598d3dd
Kristoffer Nordvik (KNORD) <KNORD@equinor.com>
2022-06-29 12:56:55 +0200

    Add black formatting

--- src/config/settings.py
+++ src/config/settings.py
@@ -3,7 +3,7 @@ from pydantic import BaseSettings, Field
 
 class Settings(BaseSettings):
     # Websocket server
-    WS_SERVER_HOST: str = Field(default='localhost')
+    WS_SERVER_HOST: str = Field(default="localhost")
     WS_SERVER_PORT: int = Field(default=5269)
 
     # Joystick limit values
@@ -11,5 +11,6 @@ class Settings(BaseSettings):
     JS_INPUT_MAX: int = Field(default=1000)
     JS_OUTPUT_MIN: int = Field(default=-32768)
     JS_OUTPUT_MAX: int = Field(default=32767)
+
 
 settings = Settings()
--- src/gamepad/emulated_gamepad.py
+++ src/gamepad/emulated_gamepad.py
@@ -1,4 +1,3 @@
-
 from numpy import interp
 from vgamepad import XUSB_BUTTON, VX360Gamepad
 
@@ -6,34 +5,51 @@ from config.settings import settings
 from controller_socket.controller_socket import ControllerSocket
 
 
-class GamepadEmulator():
+class GamepadEmulator:
     def __init__(self) -> None:
         self.virtual_gamepad = VX360Gamepad()
-        self.controller_socket = ControllerSocket(host=settings.WS_SERVER_HOST , port=settings.WS_SERVER_PORT)
+        self.controller_socket = ControllerSocket(
+            host=settings.WS_SERVER_HOST, port=settings.WS_SERVER_PORT
+        )
         self.BUTTONS: dict(str, int) = {"A": 0, "B": 0}
-        self.button_address:dict (str, XUSB_BUTTON) = {"A": XUSB_BUTTON.XUSB_GAMEPAD_A,
-        "B": XUSB_BUTTON.XUSB_GAMEPAD_B}
-        self.JOYSTICKS: dict(str, int) = {"LEFT_X": 0, "LEFT_Y": 0, "RIGHT_X": 0, "RIGHT_Y": 0}
+        self.button_address: dict(str, XUSB_BUTTON) = {
+            "A": XUSB_BUTTON.XUSB_GAMEPAD_A,
+            "B": XUSB_BUTTON.XUSB_GAMEPAD_B,
+        }
+        self.JOYSTICKS: dict(str, int) = {
+            "LEFT_X": 0,
+            "LEFT_Y": 0,
+            "RIGHT_X": 0,
+            "RIGHT_Y": 0,
+        }
         self.JOYSTICKS_INPUT_SPAN = [settings.JS_INPUT_MIN, settings.JS_INPUT_MAX]
         self.JOYSTICKS_OUTPUT_SPAN = [settings.JS_OUTPUT_MIN, settings.JS_OUTPUT_MAX]
 
     def update_joystick_values(self):
-        for count,(joystick, previous_value) in enumerate(self.JOYSTICKS.items()):
-            self.JOYSTICKS[joystick] = self.interpolate_joystick_value(value = self.controller_socket.js_values[count])
+        for count, (joystick, previous_value) in enumerate(self.JOYSTICKS.items()):
+            self.JOYSTICKS[joystick] = self.interpolate_joystick_value(
+                value=self.controller_socket.js_values[count]
+            )
 
-        self.virtual_gamepad.left_joystick(x_value=self.JOYSTICKS["LEFT_X"], y_value=self.JOYSTICKS["LEFT_Y"])
-        self.virtual_gamepad.right_joystick(x_value=self.JOYSTICKS["RIGHT_X"], y_value=self.JOYSTICKS["RIGHT_Y"])
-    
-    def interpolate_joystick_value(self,value):
-        return int(interp(x=value,xp=self.JOYSTICKS_INPUT_SPAN, fp=self.JOYSTICKS_OUTPUT_SPAN))
-  
+        self.virtual_gamepad.left_joystick(
+            x_value=self.JOYSTICKS["LEFT_X"], y_value=self.JOYSTICKS["LEFT_Y"]
+        )
+        self.virtual_gamepad.right_joystick(
+            x_value=self.JOYSTICKS["RIGHT_X"], y_value=self.JOYSTICKS["RIGHT_Y"]
+        )
+
+    def interpolate_joystick_value(self, value):
+        return int(
+            interp(x=value, xp=self.JOYSTICKS_INPUT_SPAN, fp=self.JOYSTICKS_OUTPUT_SPAN)
+        )
+
     def update_button_values(self):
-        for count,(button, previous_value) in enumerate(self.BUTTONS.items()):
+        for count, (button, previous_value) in enumerate(self.BUTTONS.items()):
             self.BUTTONS[button] = self.controller_socket.button_values[count]
             if self.BUTTONS[button] != previous_value:
                 self.actuate_virtual_button(button=button)
 
-    def actuate_virtual_button(self,button):
+    def actuate_virtual_button(self, button):
         if self.BUTTONS[button]:
             self.virtual_gamepad.press_button(button=self.button_address[button])
         else:
